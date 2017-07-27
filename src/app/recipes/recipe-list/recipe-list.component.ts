1eb416a7e4b88bd9c06fee90e1f249687b88d387
Derek <benson_wang@protonmail.com>
2017-07-27 04:08:36 -0700

    Dropdown Added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,8 +11,7 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
       new Recipe('A Test recipe', 'This is a recipe', 'https://cdn.vox-cdn.com/thumbor/Y3E0Q9Dr_' +
           'TqNCfZ08T-BOHhPXEg=/cdn.vox-cdn.com/uploads/chorus_asset/file/6695159/SwiftAndSons_beefwellington2.0.jpg'),
-      new Recipe('Another Test recipe', 'This is a recipe', 'https://cdn.vox-cdn.com/thumbor/Y3E0Q9Dr_' +
-          'TqNCfZ08T-BOHhPXEg=/cdn.vox-cdn.com/uploads/chorus_asset/file/6695159/SwiftAndSons_beefwellington2.0.jpg')
+      new Recipe('Another Test recipe', 'This is a recipe', 'http://gbc-cdn-public-media.azureedge.net/img11530.768x512.jpg')
   ];
 
   constructor() { }
